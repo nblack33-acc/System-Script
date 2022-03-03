c1b123855650b523945e906ec48288ccce704c26
The-Instructor <76235327+The-Instructor@users.noreply.github.com>
2022-03-02 22:45:40 -0700

    Create system.sh

---
+++ system.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# author: <Your Name>
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Complete the following exercises using Bash commands.
+# Create headers to label the output of each command. What information does the command output?
+# Make the output look nice.
+# Redirect the output to a file called YourName_stats.txt
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# Output the current username
+
+
+# Output the date in MM-DD-YYYY format
+
+
+# Output current users logged into the system
+
+
+# Output uptime in pretty format
+
+
+# Output summarized disk usage for all home directories on the system
